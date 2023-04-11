ee77aff9b0ed70b4c6ac2eb61ab7d7d6d9ed9294
aipilotbyjd <aicopilotideas@gmail.com>
2023-04-11 06:38:59 +0000

    DWD

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet, SafeAreaView } from "react-native";
+import { View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import {
   TextInput,
   Button,
@@ -64,49 +64,53 @@ const BusinessNameGenerator = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.inputContainer}>
+      <View style={styles.topContainer}>
         <TextInput
           label="Enter a keyword"
           value={keyword}
           onChangeText={handleKeywordChange}
           style={styles.input}
         />
-        <Button mode="contained" onPress={handleSearch}>
-          Search
+        <Button
+          mode="contained"
+          onPress={handleSearch}
+          style={styles.button}
+          labelStyle={styles.buttonLabel}
+        >
+          Generate Business Names
         </Button>
       </View>
-      <View style={styles.cardContainer}>
-        {businessNames.map((business, index) => (
-          <Card key={index} style={styles.card}>
-            <Card.Content>
-              <Title>{business.name}</Title>
-              <List.Accordion
-                title="Domain availability"
-                expanded={business.expanded}
-                onPress={() => handleAccordionToggle(index)}
-              >
-                {business.domains.map((domain, i) => (
-                  <List.Item
-                    key={i}
-                    title={domain}
-                    left={() => (
-                      <IconButton
-                        icon={domain ? "check" : "close"}
-                        color={domain ? "green" : "red"}
-                      />
-                    )}
-                  />
-                ))}
-              </List.Accordion>
-            </Card.Content>
-            <Card.Actions style={styles.cardActions}>
-              <IconButton
-                icon={business.expanded ? "minus" : "plus"}
-                onPress={() => handleAccordionToggle(index)}
-              />
-            </Card.Actions>
-          </Card>
-        ))}
+      <View style={styles.bottomContainer}>
+        <ScrollView>
+          {businessNames.map((business, index) => (
+            <Card key={index} style={styles.card}>
+              <Card.Content>
+                <Title>{business.name}</Title>
+                <List.Accordion
+                  title="Domain availability"
+                  expanded={business.expanded}
+                  onPress={() => handleAccordionToggle(index)}
+                >
+                  {business.domains.map((domain, i) => (
+                    <List.Item
+                      key={i}
+                      title={domain}
+                      left={() => (
+                        <AntDesign name="checkcircle" size={24} color="green" />
+                      )}
+                    />
+                  ))}
+                </List.Accordion>
+              </Card.Content>
+              <Card.Actions style={styles.cardActions}>
+                <IconButton
+                  icon={business.expanded ? "minus" : "plus"}
+                  onPress={() => handleAccordionToggle(index)}
+                />
+              </Card.Actions>
+            </Card>
+          ))}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -116,18 +120,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  topContainer: {
+    paddingTop: 16,
+    paddingHorizontal: 16,
+    backgroundColor: "#F6F6F6",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+    alignContent: "center",
   },
   inputContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     padding: 16,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 5,
+    marginHorizontal: 16,
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    marginTop: 1,
+  },
+  button: {
+    backgroundColor: "#6200EE",
+    borderRadius: 5,
+    alignSelf: "flex-end",
+    paddingVertical: 6,
+    width: "100%",
+    marginTop: 12,
+    marginBottom: 16,
+  },
+  buttonLabel: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 18,
   },
   input: {
     flex: 1,
-    marginRight: 16,
+    marginRight: 10,
+    width: "100%",
   },
   cardContainer: {
     flex: 1,
@@ -138,6 +176,17 @@ const styles = StyleSheet.create({
   },
   cardActions: {
     justifyContent: "flex-end",
+  },
+  bottomContainer: {
+    padding: 16,
+    backgroundColor: "#F6F6F6",
+    borderTopWidth: 1,
+    borderTopColor: "#ddd",
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  domainContainer: {
+    maxHeight: 150,
   },
 });
 
