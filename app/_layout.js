92eeb35147977388897a1cf2fe5765a05456eca8
aipilotbyjd <aicopilotideas@gmail.com>
2023-04-11 05:50:43 +0000

    sws

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
 import { useFonts, Bangers_400Regular } from "@expo-google-fonts/bangers";
-import { MochiyPopOne_400Regular } from "@expo-google-fonts/mochiy-pop-one";
 
 const _layout = () => {
   // load fonts
   const [fontsLoaded] = useFonts({
     Bangers_400Regular,
-    MochiyPopOne_400Regular,
   });
 
   if (!fontsLoaded) {
--- app/index.js
+++ app/index.js
@@ -2,15 +2,31 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { Card, TextInput } from "react-native-paper";
 
 const BussnessNameGenrator = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>hello world</Text>
-      </View>
-      <View>
-        <Text>hello world</Text>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Card>
+            <Card.Content>
+              <TextInput label="Enter your name" />
+            </Card.Content>
+          </Card>
+        </View>
+        <View style={styles.outputContainer}>
+          <Card>
+            <Card.Content>
+              <Text>Output</Text>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Text>Output</Text>
+            </Card.Content>
+          </Card>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -18,4 +34,39 @@ const BussnessNameGenrator = () => {
 
 export default BussnessNameGenrator;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  outputContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#000",
+    fontSize: 20,
+  },
+  text2: {
+    color: "#000",
+    fontSize: 20,
+  },
+  text3: {
+    color: "#000",
+    fontSize: 20,
+  },
+  text4: {
+    color: "#000",
+    fontSize: 20,
+  },
+});
